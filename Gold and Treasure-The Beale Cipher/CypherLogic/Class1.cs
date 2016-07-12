e29fe921ecb53b611abacbb5755609766ef19367
Logan Butler <logan.butler@agilethought.com>
2016-07-12 13:50:44 -0400

    Set up w/ console

---
+++ Gold and Treasure-The Beale Cipher/CypherLogic/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherLogic
+{
+    public class Cipher
+    {
+        string textString;
+        string cipherString;
+
+        public void DeCodeMessage(string text, string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gold and Treasure-The Beale Cipher/Gold and Treasure-The Beale Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CipherLogic;
+
+namespace Gold_and_Treasure_The_Beale_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cipher = new Cipher(); 
+            Console.WriteLine("Please enter the cypher Text");
+            var text = Console.ReadLine();
+            Console.WriteLine("Please enter the cypher");
+            var key = Console.ReadLine();
+
+            cipher.DeCodeMessage(text, key);
+        }
+    }
+}
