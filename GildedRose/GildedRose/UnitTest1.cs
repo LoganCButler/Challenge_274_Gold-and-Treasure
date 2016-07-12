3f639d281b28c6006fa2773732b11894a599c896
Logan Butler <logan.butler@agilethought.com>
2016-07-12 16:37:05 -0400

    Set Up

---
+++ GildedRose/GildedRose/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace GildedRose
+{
+
+    
+    
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
